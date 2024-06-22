2eaec596d861a9028518e2c71c061c9fe5cab6e0
happyprogfrog <happyprogfrog@gmail.com>
2024-06-22 23:42:06 +0900

    study: Flux, Mono 실습

---
+++ src/main/java/me/progfrog/reactorstudy/Main.java
@@ -0,0 +1,13 @@
+package me.progfrog.reactorstudy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var publisher = new Publisher();
+        // publisher.startFlux()
+        //         .subscribe(System.out::println);
+
+        publisher.startMono2()
+                .subscribe();
+    }
+}
